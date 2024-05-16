f2ff1ac2149792e917ef6c3d966ba87531cb66ff
Chris Shearer <cbshearer@gmail.com>
2024-05-15 17:01:50 -0700

    adding print to terminal option

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # qreate.py
-Simple python script to create QR codes.<br/><br/>
-Resulting .png files will be saved in the directory where script is run from. 
+Simple python script to create QR codes.
+
+## Input:
+Use option -c for the content you want in your QR Code.
+
+## Output: 
+One of the following is required:
+- Use -t or --terminal to:
+    - Print the QR Code in your terminal. 
+    - The contents of the QR code are printed as plain text above the QR code.
+- Use -f or --file to: 
+    - Output the QR code to a file (specifying the file name).
+    - This file will be placed in the current working directory.
+    - The name of the file is printed below the QR code.
+- Use -b or --both to do both of the above.
+
+## Examples:
+To just print the QR code in the terminal:
+```python
+python3 qreate.py -c https://www.bing.com -t
+```
+
+To just create the QR code in a file:
+```python
+python3 qreate.py -c https://www.bing.com -f bing.png
+```
+
+To both print the QR code in the terminal and create the QR code in a file:
+```python
+python3 qreate.py -c https://www.bing.com -b bing.png
+```
--- qreate.py
+++ qreate.py
@@ -1,20 +1,48 @@
+## following instruction here: https://towardsdatascience.com/create-and-read-qr-code-using-python-9fc73376a8f9
+## and here to print qrcodes to terminal: https://github.com/lincolnloop/python-qrcode
 ## qreate.py
-## pip install qrcode
-import qrcode
+
+import io
+import qrcode ## pip install qrcode
 import argparse
 
 ## argument setup
 parser = argparse.ArgumentParser(description="create a QR code")
+
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-f", "-file", nargs='?', const=True, help="Put QR code into a file.")
+group.add_argument("-t", "-terminal", action="store_true", help="Print QR code to screen.", default=True)
+group.add_argument("-b", "-both", nargs='?', const=True, help="Create a file and print to screen.")
+
 parser.add_argument("-c", "--content", required=True, help="Content of QR code.")
-parser.add_argument("-f", "--file", required=True, help="File path or name")
 args = parser.parse_args()
 qrinput = args.content
-qroutput = args.file
 
-## add png if not included
-if not qroutput.endswith(".png"):
-    qroutput = qroutput + ".png"
+## only print to terminal if t or b
+if not args.f or args.b:
+    print("\n\n   ", qrinput)
+    qr = qrcode.QRCode()
+    qr.add_data(qrinput)
+    f = io.StringIO()
+    qr.print_ascii(out=f)
+    f.seek(0)
+    print(f.read())
 
-# create qr code
-img=qrcode.make(qrinput)
-img.save(qroutput)
+## set output file if f or b
+if args.f:
+    qroutput = args.f
+elif args.b:
+    qroutput = args.b
+else:
+    qroutput = ""
+
+## if qroutput has data then we output a file
+if qroutput:
+    ## add png if not included
+    if not qroutput.endswith(".png"):
+        qroutput = qroutput + ".png"
+
+    ## create qr code and print file name
+    img=qrcode.make(qrinput)
+    img.save(qroutput)
+    print("   ", qroutput, "\n\n")
